70e85fe197053009361a68c8b6f8e5d094acb9c2
Carl Butz <cbutz@leepfrog.com>
2020-09-09 05:50:54 -0500

    modified app.js

--- js/app.js
+++ js/app.js
@@ -5,12 +5,12 @@ var name = prompt('Please enter Your first name:')
 
 var greeting;
 
-if(name === '')
+if(name != '' && name != null)
 {
-    greeting = '<h4 style="color: red">I am sorry, but you did not follow directions. Please refresh page and try again.</h4>';
+    greeting = '<h4> Welcome ' + name + '. Please scroll to bottom of page to review the races to select from';
 } else
 {
-    greeting = '<h4> Welcome ' + name + '. Please scroll to bottom of page to review the races to select from'
+    greeting = '<h4 style="color: red">I am sorry, but you did not follow directions. Please refresh page and try again.</h4>';
 }
 
 document.write(greeting);
