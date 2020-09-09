babf5c0f94ff13012cd1334732d598f80377fb20
Carl Butz <cbutz@leepfrog.com>
2020-09-09 05:41:30 -0500

    added second js script and user input and output

--- js/app.js
+++ js/app.js
@@ -1,5 +1,16 @@
 'use strict';
-
-
-
 console.log('This file is connected and ready to run');
+
+var name = prompt('Please enter Your first name:')
+
+var greeting;
+
+if(name === '')
+{
+    greeting = '<h4 style="color: red">I am sorry, but you did not follow directions. Please refresh page and try again.</h4>';
+} else
+{
+    greeting = '<h4> Welcome ' + name + '. Please scroll to bottom of page to review the races to select from'
+}
+
+document.write(greeting);
---
+++ js/app2.js
@@ -0,0 +1,23 @@
+'use strict';
+var race = 'none';
+window.onscroll = function(ev) {
+    if ((window.innerHeight + window.pageYOffset) >= document.body.offsetHeight && (race === 'none' || race === '' || race == null)) {
+        // alert("you're at the bottom of the page");
+
+        if(name != '')
+        {
+            race = prompt('Which race would you like to select?');
+            if(race !='' && race != null)
+            {
+                confirm('You selected ' + race);
+            } else
+            {
+                confirm('You did not select a race. Please try again.');
+            }
+            
+        } else
+        {
+            document.write('I am sorry, but you did not follow directions. Please refresh page and try again.');
+        }
+    };
+};
